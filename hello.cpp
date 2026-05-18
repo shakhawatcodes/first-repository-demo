947f496cfbab7b8c375baed88957b9b5120e0ebb
MOHAMMAD SHAKHAWAT HOSSAIN <rhosain434@gmail.com>
2026-05-18 12:34:23 +0600

    Update Calc.cpp

--- hello.cpp
+++ hello.cpp
@@ -2,6 +2,43 @@
 using namespace std;
 
 int main() {
-    cout << "Hello World";
+    double num1, num2;
+    char op;
+
+    cout << "===== C++ Calculator Pro =====" << endl;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter operator (+, -, *, /): ";
+    cin >> op;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    switch(op) {
+        case '+':
+            cout << "Result: " << num1 + num2;
+            break;
+
+        case '-':
+            cout << "Result: " << num1 - num2;
+            break;
+
+        case '*':
+            cout << "Result: " << num1 * num2;
+            break;
+
+        case '/':
+            if(num2 != 0)
+                cout << "Result: " << num1 / num2;
+            else
+                cout << "Error: Division by zero!";
+            break;
+
+        default:
+            cout << "Invalid operator!";
+    }
+
     return 0;
 }
